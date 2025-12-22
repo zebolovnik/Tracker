aa4665e42c860adb8cc49015fe184a41989ad382
NikZebolov <zebolovnik@yandex.ru>
2025-12-22 14:46:08 +1000

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Nikolay Zebolov on 22.12.2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
